ed0db44ba8c33f2ad0be69f877ca0113f9bcbed0
NUROKU <86154326+NUROKU@users.noreply.github.com>
2021-06-20 08:07:14 +0900

    Update AutoTool_Config.lua

--- VoiceAutoTool_Config/AutoTool_Config.lua
+++ VoiceAutoTool_Config/AutoTool_Config.lua
@@ -1,5 +1,5 @@
 tbl = { 
-VOICEFOLDER_PATH = [[C:\DavinciWork\20210430_Scriptテスト\VOICE]],
+VOICEFOLDER_PATH = [[C:\hogehoge]],
 AUDIO_INDEX = 1,
 VOICEBIN_NAME = [[VoiceAutoTool]],
 FHT_PATH = [[C:\ProgramData\Blackmagic Design\DaVinci Resolve\Fusion\Modules\Lua\Utf8Sjis.tbl]],
