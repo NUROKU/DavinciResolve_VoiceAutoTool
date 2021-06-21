e5736e9175315c2a42e0ceaaf4ec3ceb82f8d18e
NUROKU <shimau6@yahoo.co.jp>
2021-06-21 22:09:14 +0900

    [update]linterに合わせてそれなりに綺麗にした

--- VoiceAutoTool/AutoTool_MoveSound.lua
+++ VoiceAutoTool/AutoTool_MoveSound.lua
@@ -1,26 +1,28 @@
 -- 特定フォルダ上の音声をメディアプールにぶちこんでタイムラインに置く
 -- 音声メディアを置くBinのことを以下VoiceBinと呼ぶことにした
 
+---@diagnostic disable: undefined-global
+---↑当たり前だけどLinterがResolveのこと認識してくれないのでしかたなく
 
 --init
 CONFIG_FILE_PATH = [[C:\ProgramData\Blackmagic Design\DaVinci Resolve\Fusion\Config\VoiceAutoTool_Config\AutoTool_Config.lua]]
-config = dofile(CONFIG_FILE_PATH)
+CONFIG = dofile(CONFIG_FILE_PATH)
 
-VOICEFOLDER_PATH = config["VOICEFOLDER_PATH"]
-VOICEBIN_NAME = config["VOICEBIN_NAME"]
-FHT_PATH = config["FHT_PATH"]
-AUDIO_INDEX = config["AUDIO_INDEX"]
+VOICEFOLDER_PATH = CONFIG["VOICEFOLDER_PATH"]
+VOICEBIN_NAME = CONFIG["VOICEBIN_NAME"]
+FHT_PATH = CONFIG["FHT_PATH"]
+AUDIO_INDEX = CONFIG["AUDIO_INDEX"]
 
 
 local function GotoVBin(mediaPool)
   --CurrentBinをVoiceBinにしたうえでVoiceBinを返す
-  rootBin = mediaPool:GetRootFolder()
-  folders = rootBin:GetSubFolders()
+  local rootBin = mediaPool:GetRootFolder()
+  local folders = rootBin:GetSubFolders()
 
-  voiceBin = ""
+  local voiceBin = ""
   -- voiceBinを探すの、もっといい方法あるとおもんだけどなぁ・・・
   for folderIndex in pairs(folders) do
-    folder = folders[folderIndex]
+    local folder = folders[folderIndex]
     if folder:GetName() == VOICEBIN_NAME then
       voiceBin = folders[folderIndex]
     end
@@ -37,15 +39,15 @@ end
 
 local function PullVoiceToVBin(mediaPool)
   -- 特定フォルダ上の音声ファイル群をメディアプールに引っ張ってくる]
-  clips = mediaPool:ImportMedia(VOICEFOLDER_PATH)
+  local clips = mediaPool:ImportMedia(VOICEFOLDER_PATH)
   -- ここで音声ファイルだけ引っ張る仕様にしようと思ったけど面倒くさくなった、どーせ後でフィルタしてるし
 end
 
 
 local function isSoundRoll(clip)
   --clipが音声ファイルかどうかの確認
-  meta = clip:GetMetadata()
-  ret = false
+  local meta = clip:GetMetadata()
+  local ret = false
   if meta ~= nil then
     for a,b in pairs(meta) do
       --Sound Roll #:	hogehoge.wav
@@ -60,12 +62,12 @@ end
 
 local function PutVoiceToTimeline(mediaPool)
   --タイムラインにメディアプール上の音声ファイルを置くだけ
-  voiceBin = GotoVBin(mediaPool)
+  local voiceBin = GotoVBin(mediaPool)
 
-  clips = voiceBin:GetClips()
+  local clips = voiceBin:GetClips()
 
   for clipIndex in pairs(clips) do
-    clip = clips[clipIndex]
+    local clip = clips[clipIndex]
     if isSoundRoll(clip) then
       mediaPool:AppendToTimeline(clip)
     end
@@ -74,16 +76,16 @@ end
 
 local function MoveSoundUsecase()
   print("[Debug]MoveSound_Start-----------")
-  resolve = Resolve()
-  projectManager = resolve:GetProjectManager()
-  project = projectManager:GetCurrentProject() 
-  mediaPool = project:GetMediaPool()
+  local resolve = Resolve()
+  local projectManager = resolve:GetProjectManager()
+  local project = projectManager:GetCurrentProject() 
+  local mediaPool = project:GetMediaPool()
 
   GotoVBin(mediaPool)
   PullVoiceToVBin(mediaPool)
   print("[Debug]Sound pulled from folder")
 
-  rootBin = mediaPool:GetRootFolder()
+  local rootBin = mediaPool:GetRootFolder()
   PutVoiceToTimeline(mediaPool)
   print("[Debug]Sound put to timeline")
   print("[Debug]MoveSound End---------------")
